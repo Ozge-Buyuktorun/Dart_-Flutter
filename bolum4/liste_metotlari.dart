227d15f024590db2e543ac1409d7bbc4f71a63e9
Özge Büyüktorun <74399824+Ozge-Buyuktorun@users.noreply.github.com>
2023-02-15 12:45:07 +0300

    Update liste_metotlari.dart

--- bolum4/liste_metotlari.dart
+++ bolum4/liste_metotlari.dart
@@ -1,6 +1,6 @@
 main(List<String> args) {
   List<int> sayilar = [10, 5, 4, 3, 11, 9, 15];
-
+  //sabit boyutlu listelerde remove komutu çalışmaz.
   if (sayilar.isNotEmpty) {
     print(sayilar.first);
     print(sayilar.last);
@@ -12,21 +12,21 @@ main(List<String> args) {
 
   sayilar.add(23);
   print(sayilar);
-  sayilar.remove(3); //verilen elemanı siler
+  sayilar.remove(3); //verilen elemanı siler. ilk gördüğü 3 rakamını çıkarır.
   print(sayilar);
-  sayilar.removeAt(1);
-  print(sayilar); // verilen indeksteki elemanı siler.
+  sayilar.removeAt(1); //index =1 de ki elemanı remove eder.
+  print(sayilar); 
 
-  //sayilar.clear();
-  if (sayilar.contains(9)) {
+  //sayilar.clear(); tüm sayilar listesini temizler.
+  if (sayilar.contains(9)) { // eğer 9 içeriyorsa sayilar listesi....
     print("Listede 9 var");
   } else {
     print("Listede 9 yok");
   }
   print(sayilar);
 
-  print(sayilar.elementAt(2));
-  print(sayilar.indexOf(11));
-  sayilar.shuffle();
+  print(sayilar.elementAt(2)); //elementAt: 2inci indexteki elemanı bul.
+  print(sayilar.indexOf(11)); //  içeriğinde 11 olanın index sayısını getir. elementAt ile tam zıt çalışır.
+  sayilar.shuffle(); //Rastgele liste elemanlarının yerini değiştirir.
   print(sayilar);
 }

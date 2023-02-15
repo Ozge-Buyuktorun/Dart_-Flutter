4bd675c4a5d07c1ee482a6f7a4ad99833bc4e266
Özge Büyüktorun <74399824+Ozge-Buyuktorun@users.noreply.github.com>
2023-02-15 14:46:38 +0300

    Update koleksiyonlar_onemli_notlar.dart

--- bolum4/koleksiyonlar_onemli_notlar.dart
+++ bolum4/koleksiyonlar_onemli_notlar.dart
@@ -1,20 +1,20 @@
 void main(List<String> args) {
-  var listem = <String>[];
-  var myMap = <String, dynamic>{'yas': 34};
-  var mySet = <String>{'emre', 'hasan'};
+  var listem = <String>[];  //list
+  var myMap = <String, dynamic>{'yas': 34}; //hem key hem değer var o yüzden map.
+  var mySet = <String>{'emre', 'hasan'};  
 
-  var tekSayilar = [1, 3, 5];
-  var ciftSayilar = [2, 4, 6];
+  var tekSayilar = [1, 3, 5];  //list
+  var ciftSayilar = [2, 4, 6]; // List
 
 //spreads operator
   var sonListe = [...tekSayilar, ...ciftSayilar];
   /*  sonListe.addAll(tekSayilar);
   sonListe.addAll(ciftSayilar); */
-
+// for map
   var map1 = {'ad': 'emre'};
   var map2 = {'yas': 34};
-  var sonMap = {...map1, ...map2};
-
+  var sonMap = {...map1, ...map2};  // sonmap = {"ad","emre","yas",34} görüntülenecek.
+// for set   // eleman tekrarlarını göstermez setler.
   var set1 = {'hasan'};
   var set2 = {'emre'};
   var set3 = {'ayse'};

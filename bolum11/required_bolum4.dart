d2017c770ed88df89bbdd51611fe5ab064eed8ba
Özge Büyüktorun <74399824+Ozge-Buyuktorun@users.noreply.github.com>
2023-02-23 10:24:26 +0300

    Update required_bolum4.dart

--- bolum11/required_bolum4.dart
+++ bolum11/required_bolum4.dart
@@ -6,7 +6,8 @@
  * Son çözüm ise required kullanarak bu parametrelerin mutlaka girilmesi sağlanabilir.
  */
 
-int ucSayiyiTopla({required int ilk , required int ikinci , required int ucuncu}) {
+int ucSayiyiTopla({required int ilk , required int ikinci , required int ucuncu}) { //{} giriş opsiyonel olanlarda süslü parantez.
+//bu yöntem başka yerde işe yarardı ama burada yukarıdaki gibi olması avantajlı. int ucSayiyiTopla({int? ilk, int? ikinci, int? ucuncu}) null olma ihtimali için ? eklenebilir. 
   return ilk + ikinci + ucuncu;
 }
 
